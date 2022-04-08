76675600e21ef407cfaf215af0bad67f00e7d944
baqarmangrani <baqarmangrani@gmail.com>
2022-04-09 03:53:11 +0500

    Styled components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,8 @@
 import Landing from "./pages/Landing";
-import styled from "styled-components";
-
-const Button = styled.button`
-  background: red;
-  color: white;
-  font-size: 1rem;
-`;
-
-const ButtonSecond = styled.button`
-  background: blue;
-  color: white;
-  font-size: 1rem;
-`;
 
 function App() {
   return (
     <div>
-      <Button>CLICK ME</Button>
-      <ButtonSecond>CLICK ME</ButtonSecond>
-      <h1>jobify</h1>
       <Landing />
     </div>
   );
