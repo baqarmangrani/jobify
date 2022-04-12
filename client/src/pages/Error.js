451ab093321af85bb8562dabf9517cad41222995
baqarmangrani <baqarmangrani@gmail.com>
2022-04-13 03:06:52 +0500

    Error Page

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,4 +1,17 @@
+import errorImage from "../assets/images/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import { Link } from "react-router-dom";
+
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={errorImage} alt="not found" />
+        <h3>Ohh! page not found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
