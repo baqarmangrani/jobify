b4c35bb73cc6f3e60fc4b1c37ae4cc3ddf2cc55a
baqarmangrani <baqarmangrani@gmail.com>
2022-04-12 22:03:12 +0500

    Router config

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import Landing from "./pages/Landing";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Links,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<h1>Error</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
